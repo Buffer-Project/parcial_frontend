b11321f8d901e6a8da7576bd6979d73faf9a4bb5
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-06-10 21:02:15 -0300

    feature: Setup

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ver el archivo <code>README.md</code> para ver la consigna
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
